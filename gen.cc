a43179143e1ea38416003f6829f94b19cf36ca88
zhangqiang <zhangqiang.zqjxw73@bytedance.com>
2021-05-23 10:30:47 +0800

    fix

--- gen.cc
+++ gen.cc
@@ -1,35 +1,14 @@
 #include <bits/stdc++.h>
 #include "array.h"
+#include "tree.h"
 using namespace std;
-#define x first
-#define y second
-#define mt make_tuple
+using ll = long long;
 #define all(x) (x).begin(), (x).end()
-typedef long long ll;
 
-int xy = -1;
-
-void WriteFile(const string& data) {
-  if (xy == -1) return;
-  ofstream ofs(to_string(xy) + ".gb");
-  if (!ofs.is_open()) {
-    assertm(0, "File cant open!");
-    exit(1);
-  }
-  ofs << data << '\n';
-  ofs.close();
-}
-
-// Write your own test generating code here.
-void Generate() {
-
+void generate() {
 }
 
 int main(int argc, char** argv) {
-  if (argc == 2) {
-    // Used to write out a file for comparison.
-    xy = atoi(argv[1]);
-  }
-  Generate();
+  generate();
   return 0;
 }
